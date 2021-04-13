0054772d03e2738a0a027a28a47aa4f127221159
Sarthak Arora <sarthakarora@yahoo.com>
2021-04-13 07:50:17 +0000

    Make sightings view, html stats view, html

--- squitra/urls.py
+++ squitra/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name='squap'
 
 urlpatterns = [
-        path('map/', views.view_map, name='map'),
-        path('',views.view_main, name='main'),
+        path('map/', views.view_map, name = 'map'),
+        path('',views.view_main, name = 'main'),
+        path('sightings/',views.sightings_view, name = 'sightings'),
+        path('sightings/stats', views.stats_view, name = 'stats'),
+        path('sightings/<str:Unique_Squirrel_ID>/',views.update_squirrel_view,name='edit_sighting'),
+        path('sightings/add',views.add_squirrel_view,name='add_sighting'),
         ]
--- squitra/views.py
+++ squitra/views.py
@@ -11,4 +11,38 @@ def view_map(request):
 def view_main(request):
     return render(request,'squitra/main.html',)
 
+def sightings_view(request):
+    squirrels=Squirrel.objects.all()
+    context={
+        'squirrels':squirrels,
+        }
+    return render(request,'squitra/sightings.html',context)
+
+def stats_view(request):
+    total_count = len(Squirrel.objects.all())
+    above_ground_count = len( Squirrel.objects.filter(Location = 'Above Ground'))
+    climbing_count = len( Squirrel.objects.filter(Climbing = True))
+    running_count = len( Squirrel.objects.filter(Running = True))
+    eating_count = len( Squirrel.objects.filter(Eating = True))
+    black_count = len( Squirrel.objects.filter(Primary_Fur_Color = 'Black'))
+    gray_count = len( Squirrel.objects.filter(Primary_Fur_Color = 'Gray'))
+    cinnamon_count = len( Squirrel.objects.filter(Primary_Fur_Color = 'Cinnamon'))
+    context = {'total_count' : total_count,
+              'above_ground_count' : above_ground_count,
+              'running_count': running_count,
+              'eating_count': eating_count,
+              'climbing_count' : climbing_count,
+              'black_count' : black_count,
+              'gray_count' : gray_count,
+              'cinnamon_count' : cinnamon_count,
+              }
+    return render(request, 'squitra/stats.html', context)
+
+def update_squirrel_view(request,Unique_Squirrel_ID):
+    context = {'HI':'Hello'}
+    return render(request,'squitra/edit.html',context)
+
+def add_squirrel_view(request):
+    context = {'HI':'Hello'}
+    return render(request,'squitra/edit.html',context)
 
